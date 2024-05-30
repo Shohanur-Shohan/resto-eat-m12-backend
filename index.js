0f462e0e52947209a67b6a18af51009c2435761a
Shohanur-Shohan <shohanurshohan734@gmail.com>
2024-05-30 21:19:26 +0600

    verify admin in users/admin/email

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 //config
 require("dotenv").config();
 //middleware
@@ -25,6 +26,59 @@ async function run() {
     const menuCollection = client.db("resto_eat").collection("menu");
     const reviewsCollection = client.db("resto_eat").collection("reviews");
     const cartsCollection = client.db("resto_eat").collection("carts");
+    const usersCollection = client.db("resto_eat").collection("users");
+
+    //middleware //verifyToken
+    const verifyToken = (req, res, next) => {
+      // console.log(req.headers, "verify");
+      if (!req.headers.authorization) {
+        res.status(401).send({ message: "Forbidden access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          res.status(401).send("Forbidden access");
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    };
+
+    //jwt token
+    app.post("/jwt", async (req, res) => {
+      const userEmail = req.body?.email;
+      // console.log(userEmail);
+      const token = jwt.sign({ userEmail }, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+      });
+      res.send({ token });
+    });
+
+    app.get("/user/admin/:email", verifyToken, async (req, res) => {
+      try {
+        const email = req.params.email;
+        const decodedEmail = await req?.decoded?.userEmail;
+        if (email === decodedEmail) {
+          const query = { userEmail: email };
+          const result = await usersCollection.findOne(query);
+          const role = result?.role;
+          console.log(result);
+
+          if (role) {
+            console.log(role, email, result);
+            return res.send({ role: role });
+          } else {
+            console.log(role, email, result);
+            return res.send({ role: null });
+          }
+        }
+        return res.status(403).send({ message: "Unauthorized access" });
+      } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
 
     // all the items of menu page
     app.get("/menu", async (req, res) => {
@@ -58,11 +112,53 @@ async function run() {
       res.send(result);
     });
 
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+    //store users info
+    app.post("/users", async (req, res) => {
+      const user = await req?.body;
+      //checking user email exists or not in DB
+      const query = { userEmail: user?.userEmail };
+      const emailExists = await usersCollection.findOne(query);
+      if (emailExists) {
+        return res.send({ message: "Email already exists!", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //get all user
+    app.get("/users", verifyToken, async (req, res) => {
+      // console.log(req.headers);
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    //delete a user
+    app.delete("/users/:id", async (req, res) => {
+      const userId = req.params.id;
+      const query = { _id: new ObjectId(userId) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //role user to admin
+    app.patch("/user/admin/:id", async (req, res) => {
+      const userId = req.params.id;
+      const newRole = await req.body?.role;
+      const filter = { _id: new ObjectId(userId) };
+
+      const updateDoc = {
+        $set: {
+          role: newRole,
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
   } catch (error) {
     console.log("Server Connection Failed!");
   }
 }
 run();
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
